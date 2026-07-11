0ee0c880987391a662a6fcda5b0704b02c2df1ac
chatur446 <ayushchaturvedi1305@gmail.com>
2026-07-12 00:47:01 +0530

    Generate auth, services, and bookings modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ServicesModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
